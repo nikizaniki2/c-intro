f086fb4bd3b3b49d241d5e8cd12ec91466973f83
NikolaVoislav <nikizaniki2@gmail.com>
2021-04-30 11:14:29 +0300

    Added first struct tasks.

---
+++ struct_list.c
@@ -0,0 +1,106 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+//do one way list of structs (cars)
+//struct car has a brand, model, color and price.
+
+//func -> generate n(unkown) number of car(s)
+//func -> print_all
+//func -> delete car below given price
+
+struct Mycar
+{
+     char brand[20];
+     char model[20];
+     char color[20];
+     double price;
+};
+
+struct Mylist
+{
+    struct Mycar myCar;
+    struct Mylist *next;
+};
+
+typedef struct Mylist *car;
+
+car createCar(){
+    car temp;
+    temp = (car)malloc(sizeof(struct Mylist));
+    temp->next = NULL;
+    return temp;
+}
+
+car addCar(car root){
+    car temp, p;
+    temp = createCar();
+    printf("Brand :");
+    scanf("%s", temp->myCar.brand);
+    printf("Model :");
+    scanf("%s", temp->myCar.model);
+    printf("Color :");
+    scanf("%s", temp->myCar.color);
+    printf("Price :");
+    scanf("%lf", &temp->myCar.price);
+
+    if(root == NULL){
+        root = temp;
+    }
+    else{
+        p = root;
+        while(p->next != NULL){
+            p = p->next;
+        }
+        p->next = temp;
+    }
+    return root;
+}
+
+void print_cars(car root){
+    car current;
+    current = root->next;
+    while(current != NULL){
+        printf("brand %s, model %s, color %s, price %lf", current->myCar.brand, current->myCar.model, current->myCar.color, current->myCar.price);
+        printf("\n");
+        current = current->next;
+    }
+    free(current);
+}
+
+car deleteByPrice(car root, double price)
+{
+    while(root && root->myCar.price < price)
+    {
+        root = root->next;
+    }
+
+    car current;
+    for(current = root; current != NULL; current = current->next)
+    {
+        car temp = current->next;
+        current->next = temp->next;
+        free(temp);
+    }
+
+    free(current);
+    return root;
+}
+
+int main()
+{
+    int num = 0;
+    printf("How many cars: ");
+    scanf("%d",&num);
+    printf("\n");
+    int *root = createCar();
+    for (int i=0;i<num;i++){
+        addCar(root);
+    }
+    print_cars(root);
+    printf("Price to delete by: ");
+    double deletePrice;
+    scanf("%lf", &deletePrice);
+    deleteByPrice(root, deletePrice);
+    print_cars(root);
+    return 0;
+}
